e645f5d0e93a543d13eebcc65c00f2a4295f6bdf
Arina Taryanik <arina.taryanik@mail.ru>
2026-04-21 22:08:39 +0300

    Лабораторная №1

---
+++ lab1/index.php
@@ -0,0 +1,52 @@
+<?php
+$message = "Hello, World!";
+$buttonText = "Русский";
+
+if (isset($_GET['lang']) && $_GET['lang'] === 'ru') {
+    $message = "Привет, Мир!";
+    $buttonText = "English";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <div class="logo">
+        <img src="Logo_Polytech_rus_main.jpg" alt="Логотип" width="60">
+    </div>
+    <div class="title">
+        <h1>Лабораторная работа: "Hello, World!"</h1>
+    </div>
+</header>
+
+<main>
+    <div class="content">
+        <h1><?php echo $message; ?></h1>
+        <button onclick="changeLang()"><?php echo $buttonText; ?></button>
+    </div>
+</main>
+
+<footer>
+    <p>Задание для самостоятельной работы: Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.</p>
+</footer>
+
+<script>
+function changeLang() {
+    let url = window.location.href;
+    if (url.includes('lang=ru')) {
+        window.location.href = 'index.php';
+    } else {
+        window.location.href = 'index.php?lang=ru';
+    }
+}
+</script>
+
+</body>
+</html>
